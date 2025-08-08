db78fab502aaff3bcd4bfbb65450360af56e8c2f
Bruna Costa <brunadacosta21@yahoo.com.br>
2025-08-07 21:38:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+     <>
+  <header className='bg-indigo-950'>
+    <img src="logo.png" className='w-[60px] h-[60px]'/>
+  </header>
+
+<section>
+  <img src="banner.PNG" className='h-[250px] w-screen'/>
+</section>
+<text>aaa</text>
+
+
+
+</>
+  )
+}
+
+export default App
+
+
